53bc022cc3495f62eabeab65e53c1b0a66386176
Nicolas Saputra Gunawan <saputranicolas96@gmail.com>
2022-06-27 17:13:39 +0700

    Create flagTest.py

---
+++ Asset/bloat.py/flagTest.py
@@ -0,0 +1,6 @@
+import os
+os.system("cls")
+a = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
+            "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ "
+
+print(a[71]+a[64]+a[79]+a[79]+a[88]+a[66]+a[71]+a[64]+a[77]+a[66]+a[68])
