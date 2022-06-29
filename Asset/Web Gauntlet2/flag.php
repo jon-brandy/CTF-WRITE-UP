4d9751f53d40d740e8e73f831b3db3ab635b012f
Nicolas Saputra Gunawan <saputranicolas96@gmail.com>
2022-06-29 17:26:36 +0700

    Create flag.php

---
+++ Asset/Web Gauntlet2/flag.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["winner2"])) {
+    $_SESSION["winner2"] = 0;
+}
+$win = $_SESSION["winner2"];
+$view = ($_SERVER["PHP_SELF"] == "/filter.php");
+
+if ($win === 0) {
+    $filter = array("or", "and", "true", "false", "union", "like", "=", ">", "<", ";", "--", "/*", "*/", "admin");
+    if ($view) {
+        echo "Filters: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($win === 1) {
+    if ($view) {
+        highlight_file("filter.php");
+    }
+    $_SESSION["winner2"] = 0;        // <- Don't refresh!
+} else {
+    $_SESSION["winner2"] = 0;
+}
+
+// picoCTF{0n3_m0r3_t1m3_d5a91d8c2ae4ce567c2e8b8453305565}
+?>
