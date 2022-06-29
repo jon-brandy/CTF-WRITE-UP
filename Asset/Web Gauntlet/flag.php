966e3fd83251bec091d261f7b3f1848cc9cf172c
Nicolas Saputra Gunawan <saputranicolas96@gmail.com>
2022-06-29 17:23:28 +0700

    Create flag.php

---
+++ Asset/Web Gauntlet/flag.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["round"])) {
+    $_SESSION["round"] = 1;
+}
+$round = $_SESSION["round"];
+$filter = array("");
+$view = ($_SERVER["PHP_SELF"] == "/filter.php");
+
+if ($round === 1) {
+    $filter = array("or");
+    if ($view) {
+        echo "Round1: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($round === 2) {
+    $filter = array("or", "and", "like", "=", "--");
+    if ($view) {
+        echo "Round2: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($round === 3) {
+    $filter = array(" ", "or", "and", "=", "like", ">", "<", "--");
+    // $filter = array("or", "and", "=", "like", "union", "select", "insert", "delete", "if", "else", "true", "false", "admin");
+    if ($view) {
+        echo "Round3: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($round === 4) {
+    $filter = array(" ", "or", "and", "=", "like", ">", "<", "--", "admin");
+    // $filter = array(" ", "/**/", "--", "or", "and", "=", "like", "union", "select", "insert", "delete", "if", "else", "true", "false", "admin");
+    if ($view) {
+        echo "Round4: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($round === 5) {
+    $filter = array(" ", "or", "and", "=", "like", ">", "<", "--", "union", "admin");
+    // $filter = array("0", "unhex", "char", "/*", "*/", "--", "or", "and", "=", "like", "union", "select", "insert", "delete", "if", "else", "true", "false", "admin");
+    if ($view) {
+        echo "Round5: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($round >= 6) {
+    if ($view) {
+        highlight_file("filter.php");
+    }
+} else {
+    $_SESSION["round"] = 1;
+}
+
+// picoCTF{y0u_m4d3_1t_96486d415c04a1abbbcf3a2ebe1f4d02}
+?>
