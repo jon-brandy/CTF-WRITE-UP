a4f7fc996e9d2212468753389506003a4d181e69
Nicolas Saputra Gunawan <saputranicolas96@gmail.com>
2022-07-26 15:20:50 +0700

    Create reverse4Blocks.c

---
+++ Asset/Stonks/reverse4Blocks.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ocip{FTC0l_I4_t5m_ll0m_y_y3n5406d06dÿ¥o}
+
+int main(void)
+{
+    
+    char strings[] = {"ocip"};
+    char strings2[] = {"{FTC"};
+    char strings3[] = {"0l_I"};
+    char strings4[] = {"4_t5"};
+    char strings5[] = {"m_ll"};
+    char strings6[] = {"0m_y"};
+    char strings7[] = {"_y3n"};
+    char strings8[] = {"5406"};
+    char strings9[] = {"d06d"};
+    char strings10[] = {"ÿ¥o}"};
+    for(int i = strlen(strings) - 1; i >= 0; i--)
+    {
+        printf("%c", strings[i]);
+    }
+    for(int i =  strlen(strings2) - 1; i >= 0; i--)
+    {
+        printf("%c", strings2[i]);
+    }
+    for(int i =  strlen(strings3) - 1; i >= 0; i--)
+    {
+        printf("%c", strings3[i]);
+    }
+    for(int i =  strlen(strings4) - 1; i >= 0; i--)
+    {
+        printf("%c", strings4[i]);
+    }
+    for(int i =  strlen(strings5) - 1; i >= 0; i--)
+    {
+        printf("%c", strings5[i]);
+    }
+    for(int i =  strlen(strings6) - 1; i >= 0; i--)
+    {
+        printf("%c", strings6[i]);
+    }
+    for(int i =  strlen(strings7) - 1; i >= 0; i--)
+    {
+        printf("%c", strings7[i]);
+    }
+    for(int i =  strlen(strings8) - 1; i >= 0; i--)
+    {
+        printf("%c", strings8[i]);
+    }
+    for(int i =  strlen(strings9) - 1; i >= 0; i--)
+    {
+        printf("%c", strings9[i]);
+    }
+    for(int i =  strlen(strings10) - 1; i >= 0; i--)
+    {
+        printf("%c", strings10[i]);
+    }
+
+    return 0;
+}
